d94b7ff7cd8f9f1bac57aa98cec6e2c114684d03
nico <nperalta@gmail.com>
2015-06-15 20:57:11 -0300

    Add get_likes methods, returns how many likes the products passed by parameter have

--- app/controllers/spree/likes_controller.rb
+++ app/controllers/spree/likes_controller.rb
@@ -1,9 +1,20 @@
 class Spree::LikesController < Spree::StoreController
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   helper 'spree/products'
+  def get_likes
+    ids = params[:product_id]
+    user_favourites = []
+    user_favourites = current_user.likes.pluck(:product_id) unless current_user.nil?
+    product_favourites = Spree::Product.joins(:product_extension).joins(:likes).active.where(:id => ids).map{|e| {:product_id => e.id, :likes => e.likes.count + e.initial_favourites.to_i}}
+    render :json => {user_favourites: user_favourites.to_json, product_favourites: product_favourites}
+  end
   def like_product
-    current_user.like(product)
-    render :json => {status: 'ok', product_id: product.id}
+    if !current_user.nil?
+      current_user.like(product)
+      render :json => {status: 'ok', product_id: product.id}
+    else
+      render :json => {status: 'fail', message: 'not_logged'}
+    end
   end
 
   def unlike_product
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Spree::Core::Engine.routes.draw do
   post 'like_product/:id', to: 'likes#like_product', as: 'like_product'
   delete 'unlike_product/:id', to: 'likes#unlike_product', as: 'unlike_product'
   get '/favourites', to: 'likes#index', as: 'products_favourites'
+  get '/get_likes', to: 'likes#get_likes', as: 'get_likes'
   namespace :admin do
     get '/products/products_ranking', as: 'products_ranking'
   end
